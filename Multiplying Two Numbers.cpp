75484905c8ad5cf9601863ca9c7cf6195eb091fe
Jermaine T. Lennon <jermainetlennon2@gmail.com>
2022-10-06 13:50:17 -0400

    Add files via upload

---
+++ Multiplying Two Numbers.cpp
@@ -0,0 +1,26 @@
+// Multiplying Two Numbers
+
+// Jermaine T Lennon
+
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    double x, y, answ;
+    
+    cout << "ENTER FIRT NUMBER: " << endl;
+    cin >> x;
+    
+    cout << "ENTER SECOND NUMBER: " << endl;
+    cin >> y;
+    
+    answ = x * y;
+    
+    cout << x << " * " << y << " = " << answ << endl;
+    
+    return 0;
+    
+}
