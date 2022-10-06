77f1353ab0e469c4d5bc765f170442292fdf9108
Jermaine T. Lennon <jermainetlennon2@gmail.com>
2022-10-06 12:30:07 -0400

    Add files via upload (#1)

---
+++ Adding Two Numbers.cpp
@@ -0,0 +1,21 @@
+
+//  Basic Program: Adding Two Numbers
+
+// Jermaine T. Lennon
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1, num2, answ;
+    
+    cout << "Enter Two Numbers: " << endl;
+    cin >> num1 >> num2;
+    
+    answ = num1 + num2;
+    
+    cout << num1 << " + " << num2 << " = " << answ << endl;
+    
+    return 0;
+    
+}
